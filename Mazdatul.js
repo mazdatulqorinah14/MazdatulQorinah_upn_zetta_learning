8f9fe2afa475cdcd5bb3744b693e46c205a2d236
mazdatulqorinah14 <mazdaqorinah@gmail.com>
2022-10-28 17:14:57 +0700

    ts day 1

--- Mazdatul.js
+++ Mazdatul.js
@@ -1,10 +1,9 @@
-function pemisah(text, index1, index2){
-    var hasil = "";
-    for (var i = index1; i < index2; i++) {
+function pemisah(text, index1, index2) {
+    let hasil = "";
+    for (let i = index1; i < index2; i++) {
         hasil += text[i];
     }
     return hasil;
-
 }
 console.log(pemisah("Learnin typescript is different than javascript", 9, 19));
- 
+//# sourceMappingURL=Mazdatul.js.map
