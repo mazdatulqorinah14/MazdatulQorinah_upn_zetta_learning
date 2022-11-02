0b8524923253925938aa5c6c4d277ff9f86030d0
mazdatulqorinah14 <mazdaqorinah@gmail.com>
2022-11-02 21:08:29 +0700

    angular day 3

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { KasirComponent } from './kasir/kasir/kasir.component';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ItemInputComponent } from './item-input/item-input.component';
-import { ItemCardsComponent } from './item-cards/item-cards.component';
-import { FormsModule } from '@angular/forms';
+
 @NgModule({
-  declarations: [AppComponent, ItemInputComponent, ItemCardsComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    KasirComponent
+    
+   
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
