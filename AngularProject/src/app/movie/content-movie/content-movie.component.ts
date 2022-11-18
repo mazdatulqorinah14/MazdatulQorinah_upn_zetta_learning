9e199d2c0ceb7cc0f8d49949bed11466f24ae97d
mazdatulqorinah14 <mazdaqorinah@gmail.com>
2022-11-19 00:13:37 +0700

    Angular material day 1 selesai horee

---
+++ AngularProject/src/app/movie/content-movie/content-movie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-movie',
+  templateUrl: './content-movie.component.html',
+  styleUrls: ['./content-movie.component.scss']
+})
+export class ContentMovieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
